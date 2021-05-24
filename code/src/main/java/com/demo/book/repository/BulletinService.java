fa3c0c9df4c03129796489ed325f1153d31abe37
irenhongyan <2124483716@qq.com>
2021-05-24 08:17:01 +0800

    公告管理

---
+++ code/src/main/java/com/demo/book/repository/BulletinService.java
@@ -0,0 +1,42 @@
+package com.demo.book.repository;
+
+import com.demo.book.model.Bulletin;
+import com.demo.book.repository.BulletinMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BulletinService {
+
+    private final BulletinMapper bulletinMapper;
+
+    public BulletinService(BulletinMapper bulletinMapper) {
+        this.bulletinMapper = bulletinMapper;
+    }
+
+    public int deleteByPrimaryKey(Integer bltid) {
+        return bulletinMapper.deleteByPrimaryKey(bltid);
+    }
+
+    public int insertSelective(Bulletin record) {
+        return bulletinMapper.insertSelective(record);
+    }
+
+    public Bulletin selectByPrimaryKey(Integer bltid) {
+        return bulletinMapper.selectByPrimaryKey(bltid);
+    }
+
+    public Bulletin[] getTop3Bulletins() {
+        return bulletinMapper.getTop3Bulletins();
+    }
+
+    public Bulletin[] getAllBulletins() {
+        return bulletinMapper.getAllBulletins();
+    }
+
+    public void changeBulletin(Integer bltid, String content, Boolean valid) {
+        Bulletin bulletin = bulletinMapper.selectByPrimaryKey(bltid);
+        bulletin.setContent(content);
+        bulletin.setValid(valid);
+        bulletinMapper.updateByPrimaryKey(bulletin);
+    }
+}
